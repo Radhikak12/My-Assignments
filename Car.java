ed04b9eeb2bf0b16af09eb2223ace47fb7b61c90
Radhikak12 <116735898+Radhikak12@users.noreply.github.com>
2022-10-26 19:26:01 +0530

    Add files via upload
    Assignment1

---
+++ Car.java
@@ -0,0 +1,30 @@
+package week1day1;
+
+public class Car {
+
+		public void driveCar() {
+			System.err.println("Driving the Car");
+		}
+		public void applyBrake() {
+			System.out.println("Applying the break");
+		}
+		public void soundHorn() {
+			System.out.println("Press the horn");
+		}
+		public void isPuncture() {
+			System.out.println("The tyre got punctured");
+		}
+		public static void main(String[] args) {
+			Car obj2= new Car(); {
+		obj2.driveCar();
+		obj2.applyBrake();
+		obj2.soundHorn();
+		obj2.isPuncture();
+			}
+		
+			
+		}
+
+	}
+
+
