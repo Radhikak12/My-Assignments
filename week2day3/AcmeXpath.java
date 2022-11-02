be5253c52fbc6c87ae092616dc4d44ee44e1b890
Radhikak12 <116735898+Radhikak12@users.noreply.github.com>
2022-11-02 19:26:42 +0530

    Add files via upload

---
+++ week2day3/AcmeXpath.java
@@ -0,0 +1,35 @@
+package Week2day3;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class AcmeXpath {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver=new ChromeDriver();
+		//Open url 
+		driver.get("https://acme-test.uipath.com/login");
+// Expand the window size
+driver.manage().window().maximize();
+//Timeout for the entire session
+driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+//Enter details using advXpath
+driver.findElement(By.xpath("(//div[@class='controls']/input)[1]")).sendKeys("kumar.testleaf@gmail.com");
+driver.findElement(By.xpath("(//div[@class='controls']/input)[2]")).sendKeys("leaf@12");
+driver.findElement(By.xpath("//button[contains(text(),'Login')]")).click();
+ //Get the title of the page
+String title=driver.getTitle();
+System.out.println(title);
+//Logout 
+driver.findElement(By.xpath("//a[contains(text(),'Log Out')]/parent::li")).click();
+driver.close();
+
+}
+
+}
